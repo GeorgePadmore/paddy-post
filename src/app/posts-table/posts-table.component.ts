dd17234c26fd59502a11981d6d250c83c016918b
George Padmore Yeboah <34770919+GeorgePadmore@users.noreply.github.com>
2024-08-07 00:45:37 +0000

    Added filtering feature to the table

--- src/app/posts-table/posts-table.component.ts
+++ src/app/posts-table/posts-table.component.ts
@@ -9,17 +9,31 @@ import { Post } from '../models/post.model';
 })
 export class PostsTableComponent implements OnInit {
   posts: Post[] = [];
+  filteredPosts: Post[] = [];
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getPosts().subscribe((data: Post[]) => {
       this.posts = data;
+      this.filteredPosts = data;
     });
   }
 
   sortPosts(prop: string) {
-    this.posts = [...this.posts.sort((a, b) => (a[prop] > b[prop]) ? 1 : (b[prop] > a[prop] ? -1 : 0))];
+    this.filteredPosts = [...this.filteredPosts.sort((a, b) => (a[prop] > b[prop]) ? 1 : (b[prop] > a[prop] ? -1 : 0))];
   }
 
+  filterPosts(term: string): void {
+    if (!term) {
+      this.filteredPosts = this.posts;
+    } else {
+      this.filteredPosts = this.posts.filter(post => 
+        post.title.toLowerCase().includes(term.toLowerCase()) ||
+        post.body.toLowerCase().includes(term.toLowerCase()) ||
+        post.id.toString().includes(term) ||
+        post.userId.toString().includes(term)
+      );
+    }
+  }
 }
